639489328780bbe871bcd92fa14d0fbc92a95dd6
+ <+>
2025-04-23 20:51:22 +0300

    adds

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(id,name,age,sex,color,temperament,alive) VALUES (1,"Mr. Chocolate",6,"M","black","y",TRUE);
+INSERT INTO bears(id,name,age,sex,color,temperament,alive) VALUES (2,"Rowdy",7,"F","yellow","n",TRUE);
+INSERT INTO bears(id,name,age,sex,color,temperament,alive) VALUES (3,"Tabitha",8,"M","red","y",TRUE);
+INSERT INTO bears(id,name,age,sex,color,temperament,alive) VALUES (4,"Sergeant Brown",9,"F","green","n",TRUE);
+INSERT INTO bears(id,name,age,sex,color,temperament,alive) VALUES (5,"Melissa",10,"M","white","y",TRUE);
+INSERT INTO bears(id,name,age,sex,color,temperament,alive) VALUES (6,"Grinch",4,"F","purple","n",TRUE);
+INSERT INTO bears(id,name,age,sex,color,temperament,alive) VALUES (7,"Wendy",3,"M","grey","y",TRUE);
+INSERT INTO bears(id,name,age,sex,color,temperament,alive) VALUES (NULL,NULL,3,"F","Black","y",FALSE);
